aceb679e51ac61558f46df4bfd6181f130a5b98a
ZackarySantana <64446617+ZackarySantana@users.noreply.github.com>
2025-09-17 07:49:25 -0400

    feat: add typed htmx

--- go.mod
+++ go.mod
@@ -4,7 +4,10 @@ go 1.25.1
 
 tool github.com/a-h/templ/cmd/templ
 
-require github.com/a-h/templ v0.3.943
+require (
+	github.com/a-h/templ v0.3.943
+	github.com/will-wow/typed-htmx-go v0.2.1
+)
 
 require (
 	github.com/a-h/parse v0.0.0-20250122154542-74294addb73e // indirect
@@ -13,6 +16,7 @@ require (
 	github.com/cli/browser v1.3.0 // indirect
 	github.com/fatih/color v1.16.0 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/maragudk/gomponents v0.20.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/natefinch/atomic v1.0.1 // indirect
--- templates/index_templ.go
+++ templates/index_templ.go
@@ -8,6 +8,10 @@ package templates
 import "github.com/a-h/templ"
 import templruntime "github.com/a-h/templ/runtime"
 
+import "github.com/will-wow/typed-htmx-go/htmx"
+
+var hx = htmx.NewTempl()
+
 func Root() templ.Component {
 	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
@@ -29,7 +33,15 @@ func Root() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<!doctype html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><script src=\"/assets/htmx.min.js\"></script><link rel=\"stylesheet\" href=\"/assets/styles.css\"><title>Document</title></head><body class=\"text-yellow-500\">Something 7</body></html>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<!doctype html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><script src=\"/assets/htmx.min.js\"></script><link rel=\"stylesheet\" href=\"/assets/styles.css\"><title>Document</title></head><body class=\"text-yellow-500\"")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = templ.RenderAttributes(ctx, templ_7745c5c3_Buffer, hx.Post("/test"))
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, ">Something 7</body></html>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
