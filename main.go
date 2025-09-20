b24aed5f156fc0369cbc64f007dd938c075ea00b
ZackarySantana <64446617+ZackarySantana@users.noreply.github.com>
2025-09-20 12:51:09 -0400

    temp: main to test watcher

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/a-h/templ"
+	"github.com/fsnotify/fsnotify"
+	"github.com/zackarysantana/goview/internal/watcher"
+
+	"github.com/zackarysantana/goview/stats"
 	"github.com/zackarysantana/goview/template"
 )
 
-func main() {
+func Main() {
 	http.Handle("/", templ.Handler(template.Index()))
 
 	http.Handle("/assets/",
@@ -17,4 +23,30 @@ func main() {
 
 	fmt.Println("Listening on :3000 (the proxy is on :7331)")
 	http.ListenAndServe(":3000", nil)
+}
+
+func main() {
+	fs := os.DirFS(".")
+	module, err := stats.ParseModule(fs, ".")
+	if err != nil {
+		fmt.Println("Error parsing module:", err)
+		return
+	}
+
+	fmt.Printf("Module: %+v\n", module.GoMod)
+
+	watcher, err := watcher.NewWatcher(context.Background(), ".", []string{".git", "node_modules"})
+	if err != nil {
+		fmt.Println("Error creating watcher:", err)
+		return
+	}
+
+	err = watcher.Watch(context.Background(), ".", func(event fsnotify.Event) {
+		fmt.Printf("File changed: %s\n", event.Name)
+		module.Reload(fs, event.Name)
+	})
+	if err != nil {
+		fmt.Println("Error watching files:", err)
+		return
+	}
 }
