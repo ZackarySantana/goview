218a64801cc2ba8f467a79fc38a1b915d013a409
ZackarySantana <64446617+ZackarySantana@users.noreply.github.com>
2025-09-20 12:50:46 -0400

    feat: allow module to reload a path

--- stats/module.go
+++ stats/module.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"io/fs"
 	"path"
+	"slices"
+	"strings"
 )
 
 type PackageAndTests struct {
@@ -14,6 +16,70 @@ type PackageAndTests struct {
 type Module struct {
 	GoMod    *GoMod
 	Packages []*PackageAndTests
+}
+
+func (m *Module) Reload(filesystem fs.FS, path string) error {
+	if strings.HasSuffix(path, "go.mod") {
+		fmt.Println("Reloading go.mod")
+		return m.reloadGoMod(filesystem, path)
+	}
+
+	if strings.HasSuffix(path, ".go") {
+		fmt.Println("Reloading go file:", path)
+		return m.reloadGoFile(filesystem, path)
+	}
+
+	// otherwise, it's a directory and we should parse the whole package again.
+
+	return nil
+}
+
+func (m *Module) reloadGoMod(filesystem fs.FS, fileName string) error {
+	goModFile, err := filesystem.Open(fileName)
+	if err != nil {
+		return fmt.Errorf("opening file '%s': %w", fileName, err)
+	}
+	goMod, err := ParseGoMod(goModFile)
+	if err != nil {
+		return fmt.Errorf("parsing file '%s': %w", fileName, err)
+	}
+	m.GoMod = goMod
+	return nil
+}
+
+func (m *Module) reloadGoFile(filesystem fs.FS, path string) error {
+	directoryName := path[:strings.LastIndex(path, "/")]
+	fileName := path[strings.LastIndex(path, "/")+1:]
+	for _, pkgAndTests := range m.Packages {
+		fmt.Println("Checking package:", pkgAndTests.Pkg.Directory)
+		if pkgAndTests.Pkg.Directory != directoryName {
+			continue
+		}
+		if !slices.Contains(pkgAndTests.Pkg.GoFiles, fileName) {
+			continue
+		}
+		testFile, err := filesystem.Open(path)
+		if err != nil {
+			return fmt.Errorf("opening file '%s': %w", path, err)
+		}
+		parsedTestFile, err := ParseTestFile(testFile)
+		if err != nil {
+			return fmt.Errorf("parsing file '%s': %w", path, err)
+		}
+		parsedTestFile.Name = path
+		parsedTestFile.Path = path
+
+		for _, existingTest := range pkgAndTests.Tests {
+			if existingTest.Name == parsedTestFile.Name {
+				*existingTest = *parsedTestFile
+				return nil
+			}
+		}
+
+		return nil
+	}
+
+	return nil
 }
 
 func ParseModule(filesystem fs.FS, dirPath string) (*Module, error) {
